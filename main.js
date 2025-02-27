06705e865976e598764a72b3c0363b601b94628f
Eliel Corona <elielcoronam@gmail.com>
2025-02-27 14:19:52 -0600

    Inicio

---
+++ main.js
@@ -0,0 +1,15 @@
+//Programa donde el usuario realice una suma
+alert('Hagamos una suma')
+
+//Ingreso de datos
+let num1 = Number(prompt('Ingresa numero:'))
+let num2 = Number(prompt('Ingresa el segundo numero:'))
+
+
+//Operacion
+
+let result = num1 + num2
+
+//Mostrar resultado
+
+console.log('El resultado de ' + num1 + ' + ' + num2 + ' = ' + result);
